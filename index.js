5fb683f2310679f226b604b8144a01b5b59bdaa6
Virgilio Gómez <virgilio@redradix.com>
2022-06-15 18:41:46 +0200

    :sparkles: first steps with sockets

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ const express = require('express')
 const path = require('path')
 
 const app = express()
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
+
+app.use(express.json())
+
 const port = 3003
+
+let socketInstance
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(`${__dirname}/views/semaphore/index.html`))
@@ -12,6 +19,17 @@ app.get('/styles.css', (req, res) => {
   res.sendFile(path.join(`${__dirname}/views/semaphore/styles.css`))
 })
 
-app.listen(port, () => {
+app.post('/message', (req, res) => {
+  if (socketInstance) {
+    socketInstance.emit(req.body.event, req.body.message)
+  }
+  res.end()
+})
+
+io.of('/admin').on('connection', function (socket) {
+  socketInstance = socket
+})
+
+http.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
