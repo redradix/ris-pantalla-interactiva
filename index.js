639fce2aa005c78fe310dd62dc9d0b264531480b
Virgilio Gómez <virgilio@redradix.com>
2022-06-15 11:38:19 +0200

    :tada: Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+const port = 3003
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(`${__dirname}/views/semaphore/index.html`))
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
