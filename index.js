2562af978a4fbd56c49279b369b2a5fb0266a665
Sonia Ruiz <yune.vk@gmail.com>
2022-06-16 17:35:30 +0200

    💩 Bad code

--- index.js
+++ index.js
@@ -5,37 +5,30 @@ const app = express()
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
 
-const dynamicStatic = require('express-dynamic-static')()
-
 app.use(express.json())
-app.use(dynamicStatic)
 
 const port = 3003
 
 let socketInstance
 let activeApp
 
+app.use('/', express.static(path.join(`${__dirname}/views/welcome`)))
 app.use('/dashboard', express.static(path.join(`${__dirname}/views/dashboard`)))
-
-app.use('/', (req, res, next) => {
-  // TODO: refactor
-  if (activeApp) {
-    dynamicStatic.setPath(path.join(`${__dirname}/views/apps/semaphore`))
-  } else {
-    dynamicStatic.setPath(path.join(`${__dirname}/views/welcome`))
-  }
-
-  next()
-})
+app.use(
+  '/semaphore',
+  express.static(path.join(`${__dirname}/views/apps/semaphore`)),
+)
 
 app.get('/', (req, res) => {
-  const route = activeApp ? `/views/apps/${activeApp}` : '/views/welcome'
-
   res.sendFile(path.join(`${__dirname}${route}/index.html`))
 })
 
 app.get('/dashboard', (req, res) => {
   res.sendFile(path.join(`${__dirname}/views/dashboard/index.html`))
+})
+
+app.get('/semaphore', (req, res) => {
+  res.sendFile(path.join(`${__dirname}/views/apps/semaphore/index.html`))
 })
 
 app.post('/message', (req, res) => {
@@ -47,7 +40,7 @@ app.post('/message', (req, res) => {
 
 app.post('/update-app', (req, res) => {
   activeApp = activeApp ? null : req.body.app
-  socketInstance.emit('reload')
+  socketInstance.emit('reload', activeApp || '/')
   res.end()
 })
 
